3032cf2529d1cd13c24679a2be340b5314dc45eb
Mauri Korn <mauri.korn@sou.inteli.edu.br>
2025-08-22 22:20:03 +0000

    fix: adiciona  no main.py e corrige NameError ao rodar testes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import json
 import mean_var_std
-from unittest import main as unittest_main
+from unittest import main
 
 # Saída legível
 print(json.dumps(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]), indent=2))
