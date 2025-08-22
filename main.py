1e909c8b44d22490008f42953d99d982d4111827
Mauri Korn <mauri.korn@sou.inteli.edu.br>
2025-08-22 22:18:05 +0000

    chore: melhora legibilidade no main.py — imprime dicionário com indentação e executa testes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This entrypoint file to be used in development. Start by reading README.md
+import json
 import mean_var_std
-from unittest import main
+from unittest import main as unittest_main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+# Saída legível
+print(json.dumps(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]), indent=2))
 
-# Run unit tests automatically
+# Rodar testes locais
 main(module='test_module', exit=False)
